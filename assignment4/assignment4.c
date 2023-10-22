fd79c5c68fa9683d5a161ded1c0887f54d25ab01
Doctorxsx <allen31042@gmail.com>
2023-10-23 07:05:33 +0800

    Assignment4

---
+++ assignment4/assignment4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+int main()
+{
+  time_t t = time(0);
+  struct tm tm;
+  char buf[256];
+  setenv("TZ", ":Asia/Taipei", 1); tzset();
+  localtime_r(&t, &tm);
+  strftime(buf, sizeof(buf), "%a %b %X %d %Z %G", &tm);
+  printf("%s\n", buf);
+  setenv("TZ", ":Pacific/Auckland", 1); tzset();
+  localtime_r(&t, &tm);
+  strftime(buf, sizeof(buf), "%a %b %X %d %Z %G", &tm);
+  printf("%s\n", buf);
+}
