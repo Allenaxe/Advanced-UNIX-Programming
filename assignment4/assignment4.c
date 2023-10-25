587c15f5dd1b5b77254a1ed6ddbd255075484834
Doctorxsx <allen31042@gmail.com>
2023-10-25 13:34:32 +0800

    Assignment4

--- assignment4/assignment4.c
+++ assignment4/assignment4.c
@@ -11,10 +11,10 @@ int main()
   char buf[256];
   setenv("TZ", ":Asia/Taipei", 1); tzset();
   localtime_r(&t, &tm);
-  strftime(buf, sizeof(buf), "%a %b %X %d %Z %G", &tm);
+  strftime(buf, sizeof(buf), "%a %b %d %X %Z %G", &tm);
   printf("%s\n", buf);
   setenv("TZ", ":Pacific/Auckland", 1); tzset();
   localtime_r(&t, &tm);
-  strftime(buf, sizeof(buf), "%a %b %X %d %Z %G", &tm);
+  strftime(buf, sizeof(buf), "%a %b %d %X %Z %G", &tm);
   printf("%s\n", buf);
 }
